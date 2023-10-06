949ec5f296d733ac76d228480f8dd438f17b0891
Andrew Long <app@anaru.nz>
2023-10-06 03:14:21 +0000

    Updated 1 file via CloudCannon.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,7 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
   devtools: { enabled: true },
-  //ssr: true,
+  ssr: true,
 
-  modules: [
-    // Remove it if you don't use Plausible analytics
-    // https://github.com/nuxt-modules/plausible
-   // '@nuxtjs/plausible'
-  ]
+  modules: ['@nuxt/content']
 })
